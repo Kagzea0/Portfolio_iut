00bf9349984ecc89aca68a046e727da3cef35af7
kagzea0 <erwan.michaudwinkler@gmail.com>
2022-11-15 09:58:13 +0100

    com13

--- formulaire.php
+++ formulaire.php
@@ -1,21 +1,16 @@
-<?php 
-    if($_POST) {
-        if($_POST['nom']!='') {
-        echo "<br/><br/>Bonjour " . $_POST['titre'] . " " . $_POST['nom'] . "!<br/>";
-        echo "Je peux vous appeler " . $_POST['prenom'] . "<br/><br/>";
-
-            if(isset($_POST['bDebutant'])) {
-            echo "C'est une bonne idée de commencer à apprendre à programmer en PHP !<br/><br/>";
-            }
-                else {
-                    if($_POST['sexe']=='H') {
-                        $mot = "débutant";
-                    }
-                else {
-                    $mot = "débutante";
-                }
-                echo "Comme vous n'êtes pas " . $mot . " vous pouvez passer directement au mini-projet !<br/><br/>";
-            }
-        }
-    }
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php print "Merci pour votre message ";
+    print $_POST["nom"] ;
+    print " ";
+    print $_POST["prenom"];
+    ?>
+</body>
+</html>
