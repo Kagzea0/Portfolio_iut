08aaa3b80ea77bf971392558814b6ab788917381
kagzea0 <erwan.michaudwinkler@gmail.com>
2022-11-15 11:26:22 +0100

    com15

--- formulaire.php
+++ formulaire.php
@@ -4,12 +4,11 @@
   <title>rÃ©ponse du serveur</title>
   </head>
   <body>
-    
-  <?php print "Merci pour votre message ";
-  print $_POST["nom"] ;
-  print " ";
-  print $_POST["prenom"];
-  ?>
+        <?php print "Merci pour votre message ";
+        print $_POST["nom"] ;
+        print " ";
+        print $_POST["prenom"];
+        ?>
     <p><a href='index.html'> Go back to the homepage</a></p>
   </body>
 </html> 
