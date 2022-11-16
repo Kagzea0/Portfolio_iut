caaafc2f9cfcce72cfbb5ce57315af015a51a853
Kagzea <erwan.michaudwinkler@gmail.com>
2022-11-16 21:44:19 +0100

    com14

--- formulaire.php
+++ formulaire.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html>
 <html>
   <head>
-  <title>rÃ©ponse du serveur</title>
+  <title>réponse du serveur</title>
   </head>
-  <body>
+  <body bgcolor="#222436">
         <?php print "Merci pour votre message ";
         print $_POST["nom"] ;
         print " ";
         print $_POST["prenom"];
         ?>
-    <p><a href='index.html'> Go back to the homepage</a></p>
+    <p style="display: flex; justify-content: center; vertical-align:middle;">
+      <a href='index.html' 
+      style="text-decoration: underline #d679ed; 
+      font-size: 25px; 
+      color: #c4bcbc; 
+      font-family: Gill Sans, sans-serif;
+      font-weight: 900;
+      font-size: 60px;
+      position: absolute;
+      top: 40%;"> 
+      Back to the homepage</a>
+    </p>
   </body>
 </html> 
